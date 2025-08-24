9535e12b081b10a0395cadae7bb1dbe59bee30e7
Everlasting-dev <78971593+Everlasting-dev@users.noreply.github.com>
2025-08-24 18:25:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ let lastFile = null;
 function showToast(msg, type="error"){
   els.toast.textContent = msg;
   els.toast.classList.remove("hidden");
+  els.toast.style.display = "block";   // <-- ensure it shows again
   els.toast.style.background = (type==="error") ? "#3b0b0b" : "#0b3b18";
   els.toast.style.borderColor = (type==="error") ? "#742020" : "#1a6a36";
   clearTimeout(showToast._t);
